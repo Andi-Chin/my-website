64cac3dfb89c46e5109499009b89b8246644e73c
andichin <hellocyhandy@gmail.com>
2019-02-14 11:53:19 -0500

    new stuf

---
+++ game.js
@@ -0,0 +1,106 @@
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "#FF0000";
+
+
+ctx.fillRect(0, 0, 150, 75);
+ctx.moveTo(0, 0);
+ctx.lineTo(100, 150);
+ctx.stroke();
+
+
+//half a circle
+ctx.beginPath();
+ctx.arc(300, 300, 50, Math.PI / 2, Math.PI);
+ctx.stroke();
+
+var grd = ctx.createLinearGradient(0, 0, 200, 0);
+grd.addColorStop(0, "red");
+grd.addColorStop(1, "white");
+
+ctx.fillStyle = grd;
+ctx.fillRect(0, 300, 200, 100);
+
+ctx.font = "30px Arial";
+ctx.fillStyle = 'green';
+ctx.textAlign = 'center';
+ctx.fillText("Hello World", 300, 50);
+
+
+
+
+window.onload = function() {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    var img = document.getElementById("mineblox");
+    ctx.drawImage(img, 0, 450);
+    drawClock();
+    setInterval(drawClock, 1000);
+}
+
+//drawing a clock
+function drawClock() {
+	ctx.fillStyle = "#AC22FF";
+	ctx.fillRect(500, 100, 300, 300);
+
+	var clockX = 500 + 150;
+	var clockY = 100 + 150;
+
+	//clock outer ring
+	drawCircle(clockX, clockY, 145, '#AAAAAA');
+	//inner ring
+	drawCircle(clockX, clockY, 140, '#888888');
+	//dot in the middle
+	drawCircle(clockX, clockY, 20, '#555555');
+
+	drawNumbers();
+
+	drawHands();
+}
+
+
+function drawCircle(x, y, r, color) {
+	ctx.beginPath();
+	ctx.arc(x, y, r, 0, Math.PI * 2);
+	ctx.fillStyle = color;
+	ctx.fill();
+}
+
+const angle30 = Math.PI / 6;
+const angle90 = Math.PI / 2;
+
+function drawNumbers() {
+	
+	for (var i = 1; i < 13; i ++) {
+		ctx.fillText(i, 
+			Math.cos(i * angle30 - angle90) * 100 + 650, 
+			Math.sin(i * angle30 - angle90) * 100 + 250);
+	}
+}
+
+
+function drawHands() {
+	var now = new Date();
+	var hour = now.getHours();
+	var minute = now.getMinutes();
+	var second = now.getSeconds();
+
+	hand(hour % 12);
+	hand(minute / 5);
+	hand(second / 5);
+
+	// console.log(now);
+
+	function hand(timeValue) {
+		ctx.moveTo(650, 250);
+		var xPos = Math.cos(timeValue * angle30 - angle90) * 100 + 650;
+		var yPos = Math.sin(timeValue * angle30 - angle90) * 100 + 250;
+		ctx.lineTo(xPos, yPos);
+		ctx.stroke();
+	}
+
+}
+
+console.log('reached the end of the program');
+
--- script.js
+++ script.js
@@ -1,16 +1,20 @@
+
 function myMove() {
   var elem = document.getElementById("animate");   
   var pos = 0;
   var id = setInterval(frame, 5);
+
+  console.log('id: ' + id);
+  id = setInterval(frame, 5);
   function frame() {
-    if (pos == 350) {
+    if (pos == 700) {
       clearInterval(id);
     } else {
       pos++; 
       elem.style.top = pos + "px"; 
       elem.style.left = pos + "px";
-      console.log("top: " + elem.style.top);
-      console.log("left: " + elem.style.left);
+      // console.log("top: " + elem.style.top);
+      // console.log("left: " + elem.style.left);
     }
   }
 }
@@ -24,7 +28,33 @@ function objectDemonstration() {
 	    return this.language;
 	  }
 	};
+
+	Object.defineProperty(person, "test", {
+	  get : function () {
+	  	this.lastname = 'alt lastname'; 
+	  	return this.lastname;
+	  }
+	});
+	console.log(person.test);
+	console.log(person);
 	document.getElementById("demo").innerHTML = person.lang;
 
 }
+
+function Person(firstName, lastName, language) {
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.language = language;
+	this.name = function() {
+		return this.firstName + ' ' + this.lastName;
+	}
+}
+Person.prototype.calcAge = function (birthYear) {
+	age = 2019 - birthYear;
+	this.age = age;
+	return age;
+}
+var andy = new Person('Andy', 'Chen', 'en');
+
+
 objectDemonstration();
